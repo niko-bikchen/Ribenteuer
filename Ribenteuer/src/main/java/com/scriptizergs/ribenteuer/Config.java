713ce4d969c7aa92a46091e800ea0fbe40358658
Vlad <super.gaponenko2000@gmail.com>
2020-03-15 18:08:10 +0200

    starting configuration

---
+++ Ribenteuer/src/main/java/com/scriptizergs/ribenteuer/Config.java
@@ -0,0 +1,28 @@
+//developed by Vladyslav Haponenko
+package com.scriptizergs.ribenteuer;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+
+//the class establishes connection to mongo db
+@Configuration
+@EnableMongoRepositories(basePackages = "model")
+public class Config {
+
+
+    //connection to database
+    @Bean
+    public static MongoClient mongo() {
+        return new MongoClient(new MongoClientURI("mongodb://Vladyslav:Nikitabeliy2012@clusterfar-shard-00-00-7itsr.mongodb.net:27017,clusterfar-shard-00-01-7itsr.mongodb.net:27017,clusterfar-shard-00-02-7itsr.mongodb.net:27017/test?ssl=true&replicaSet=clusterfar-shard-0&authSource=admin&retryWrites=true&w=majority"));
+    }
+
+    //choosing db
+    @Bean
+    public static MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongo(), "rt");
+    }
+}
---
+++ Ribenteuer/src/main/java/com/scriptizergs/ribenteuer/RibenteuerApplication.java
@@ -0,0 +1,13 @@
+package com.scriptizergs.ribenteuer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibenteuerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibenteuerApplication.class, args);
+    }
+
+}
