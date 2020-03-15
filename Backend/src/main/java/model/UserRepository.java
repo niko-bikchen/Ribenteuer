fd2a73dafba70e17d9de335a6ae53c80998c6d4a
Vlad <super.gaponenko2000@gmail.com>
2020-03-15 18:36:30 +0200

    add GITKEEP!!!

--- Backend/src/main/java/model/UserRepository.java
+++ Backend/src/main/java/model/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String>{
 
-    public User findByEmail(String email);
+    public User findByMail(String mail);
 }
